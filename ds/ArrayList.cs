c786560917d042461b92409b07a416eae4bac90f
im-yosu <yosuke.oda@outlook.com>
2022-11-20 15:56:02 -0500

    arraylist shell

---
+++ ds/ArrayList.cs
@@ -0,0 +1,21 @@
+// resizable array
+public class ArrayList<T>
+{
+    // data holder
+    public ArrayList()
+    {
+        
+    }
+
+    // add
+
+    // get (by index)
+
+    // set (change)
+
+    // remove
+
+    // clear
+
+    // size
+}
