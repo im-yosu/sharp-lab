d60c8f65d05b2b6cdd6f8bdcbcbf0c69eaa0bff5
im-yosu <yosuke.oda@outlook.com>
2022-11-20 17:10:51 -0500

    prep add

--- ds/ArrayList.cs
+++ ds/ArrayList.cs
@@ -1,13 +1,24 @@
-// resizable array
+// auto resizing array
 public class ArrayList<T>
 {
     // data holder
+    private T[] _arrayList;
+
     public ArrayList()
     {
-        
+        // start out with 10
+        _arrayList = new T[10];
     }
 
-    // add
+    // add - no need to return 
+    // TODO provide mechanism to grow (iirc if filled halfway then double the size)
+    public void Add(T item) {
+        // check for size (implement the method first)
+
+        // determine if the size needs adjustment (double check when to double the size)
+
+        // add the item 
+    }
 
     // get (by index)
 
